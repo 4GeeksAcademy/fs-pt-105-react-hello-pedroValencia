0236a0712fe4e49e355076b41923cf4041b92765
Pedro Valencia Alves <pedrovalenciaalves98@gmail.com>
2025-05-09 20:16:00 +0000

    proyecto finalizado

---
+++ src/js/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return(
+        <div className="bg-dark-subtle w-100 pt-5" style={{height:"10rem"}}>
+            <p>Copyright ©️ Your website 2018</p>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,11 @@
+const Jumbotron = () => {
+    return (
+        <div className="jumbotron">
+            <h1>Titutlo</h1>
+            <p>Hola esto es un párrafo</p>
+            <button className="btn btn-primary">Call to action!</button>
+        </div>
+    )
+}
+
+export default Jumbotron;
